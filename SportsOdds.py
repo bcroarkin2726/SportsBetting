b4920bd03a011f4574d0d3c7f8f4bca06c8accea
Brandon Croarkin <brc2xw@virginia.edu>
2019-12-17 20:11:58 -0500

    Update SportsOdds.py

--- SportsOdds.py
+++ SportsOdds.py
@@ -721,17 +721,16 @@ if multipleDataPulls(nflodds, NFL_Week):
     </html>
     """
     
-    # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(html_email_message, "html")
-    
     # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
-    message.attach(part1)
+    message.attach(MIMEText(html_email_message, "html"))
     
+    # Email recipient list
+    email_list = ["loudoun5@yahoo.com", "dangeloreategui@gmail.com"]
+            
     # Create secure connection with server and send email
     context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(
-            sender_email, receiver_email, message.as_string()
-        )
+    
+    for receivier_email in email_list:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
